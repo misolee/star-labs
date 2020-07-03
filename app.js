afd8087ac2a6ffad38cc20229e1865f08a98b261
Miso Lee <mzxmiso@gmail.com>
2020-07-02 22:08:43 -0700

    app.get

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const db = require('./config/keys').mongoURI
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
-
 const users = require('./routes/api/users')
 
-app.get('/', (req, res) => res.send('Hello world'))
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("frontend/build"));
+	app.get("/", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+	});
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server is running on port ${port}`))
@@ -28,10 +32,3 @@ app.use(passport.initialize())
 require('./config/passport')(passport);
 
 app.use('/api/users', users)
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
